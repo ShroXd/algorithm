01067127cfa58edac9ccdf1836b4cb0a0dce7bef
ShroXd <ShroXdF@gmail.com>
2022-07-13 22:25:26 +0800

    feat: implement basic code for MaxPriorityQueueArray

--- src/main/kotlin/sort/priorityQueues.kt
+++ src/main/kotlin/sort/priorityQueues.kt
@@ -1,12 +1,40 @@
 package sort
 
-interface MaxPriorityQueue<Key : Comparable<Key>> {
-    val size: Int
-    val array: Array<Key>
-
-    fun insert(v: Key)
-    fun max(): Key
-    fun delMax(): Key
+// TODO: make it generic
+interface MaxPriorityQueue {
+    fun insert(v: Int)
+    fun max(): Int
+    fun delMax(): Int
     fun isEmpty(): Boolean
-    fun size(): Int
+}
+
+class MaxPriorityQueueArray() : MaxPriorityQueue {
+    var size: Int = 10
+        private set
+    private var queue: Array<Int> = Array(this.size) { 0 }
+
+    constructor(max: Int) : this() {
+        this.size = max
+        this.queue = Array(this.size) { 0 }
+    }
+
+    constructor(queue: Array<Int>) : this() {
+        this.size = queue.size
+        this.queue = queue
+    }
+
+    override fun isEmpty(): Boolean = this.size == 0
+
+    override fun insert(v: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun max(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun delMax(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
