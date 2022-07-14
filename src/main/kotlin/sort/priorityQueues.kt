817a0e232bb3bd86516eff399bc6317392a08ae0
ShroXd <ShroXdF@gmail.com>
2022-07-14 21:35:00 +0800

    feat: implement insert / max / delMax of priority queue

--- src/main/kotlin/sort/priorityQueues.kt
+++ src/main/kotlin/sort/priorityQueues.kt
@@ -2,15 +2,18 @@ package sort
 
 // TODO: make it generic
 interface MaxPriorityQueue {
+    fun isEmpty(): Boolean
     fun insert(v: Int)
     fun max(): Int
     fun delMax(): Int
-    fun isEmpty(): Boolean
 }
 
-class MaxPriorityQueueArray() : MaxPriorityQueue {
-    var size: Int = 10
+class MaxPriorityQueueUnorderedArray() : MaxPriorityQueue {
+    var capacity: Int = 10
         private set
+    var size: Int = 0
+        private set
+
     private var queue: Array<Int> = Array(this.size) { 0 }
 
     constructor(max: Int) : this() {
@@ -26,15 +29,41 @@ class MaxPriorityQueueArray() : MaxPriorityQueue {
     override fun isEmpty(): Boolean = this.size == 0
 
     override fun insert(v: Int) {
-        TODO("Not yet implemented")
+        if (this.size / this.capacity > 1.5) {
+            val temp = Array(this.size * 2) { 0 }
+
+            for ((el, idx) in this.queue.withIndex()) {
+                temp[idx] = el
+            }
+
+            this.queue = temp.clone()
+        }
+
+        this.queue[size++] = v
     }
 
     override fun max(): Int {
-        TODO("Not yet implemented")
+        var currentMax = this.queue[0]
+
+        for (el in this.queue.drop(1))
+            if (el > currentMax)
+                currentMax = el
+
+        return currentMax
     }
 
     override fun delMax(): Int {
-        TODO("Not yet implemented")
-    }
+        var currentMaxIdx = this.queue.size - 1
 
+        for (idx in this.queue.size - 2 downTo 0)
+            if (this.queue[idx] > this.queue[currentMaxIdx])
+                currentMaxIdx = idx
+
+        swap(this.queue, currentMaxIdx, this.queue.size - 1)
+
+        val max = this.queue.last()
+        this.queue[this.size--] = 0
+
+        return max
+    }
 }
