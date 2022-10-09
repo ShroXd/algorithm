2dfcdd66ce717303dc14a47776b944d20dfc5e20
ShroXd <ShroXdF@gmail.com>
2022-10-09 20:56:13 +0800

    feat: fix up put & get method

--- src/main/kotlin/search/RedBlackBST.kt
+++ src/main/kotlin/search/RedBlackBST.kt
@@ -1,14 +1,12 @@
 package search
 
-import java.security.Key
-
 
 class RedBlackBST<K : Comparable<K>, V> {
     enum class Color {
         RED, BLACK
     }
 
-    class Node<K, V>(val key: K, var value: V, var color: Color = Color.BLACK, var size: Int = 0) {
+    class Node<K, V>(var key: K, var value: V, var color: Color = Color.BLACK, var size: Int = 0) {
         var left: Node<K, V>? = null
         var right: Node<K, V>? = null
     }
@@ -24,56 +22,37 @@ class RedBlackBST<K : Comparable<K>, V> {
     fun isEmpty(): Boolean =
         root == null
 
-    // Search
-    private fun get(key: K, node: Node<K, V>?): Node<K, V>? {
-        if (node == null) {
-            return null
-        }
-
-        val gap: Int = key.compareTo(node.key)
-        if (gap > 0) {
-            get(key, node.right)
-        } else if (gap < 0) {
-            get(key, node.left)
-        } else {
+    private fun rotateLeft(node: Node<K, V>): Node<K, V> {
+        if (node.right == null) {
             return node
         }
+        val temp = node.right!!
 
-        return null
-    }
+        node.right = temp.left
+        temp.left = node
 
-    fun get(key: K): Node<K, V>? {
-        return get(key, root)
-    }
-    fun contains(key: K): Boolean {
-        return get(key) != null
-    }
-
-    private fun rotateLeft(node: Node<K, V>): Node<K, V>? {
-        val temp = node.right
-
-        node.right = temp?.left
-        temp?.left = node
-
-        temp?.color = node.color
+        temp.color = node.color
         node.color = Color.RED
 
-        temp?.size = node.size
+        temp.size = node.size
         node.size = size(node.left) + size(node.right) + 1
 
         return temp
     }
 
-    private fun rotateRight(node: Node<K, V>): Node<K, V>? {
-        val temp = node.left
+    private fun rotateRight(node: Node<K, V>): Node<K, V> {
+        if (node.left == null) {
+            return node
+        }
+        val temp = node.left!!
 
-        node.left = temp?.right
-        temp?.right = node
+        node.left = temp.right
+        temp.right = node
 
-        temp?.color = node.color
+        temp.color = node.color
         node.color = Color.RED
 
-        temp?.size = node.size
+        temp.size = node.size
         node.size = size(node.left) + size(node.right) + 1
 
         return temp
@@ -87,35 +66,148 @@ class RedBlackBST<K : Comparable<K>, V> {
         return node
     }
 
+    private fun moveRedLeft(node: Node<K, V>): Node<K, V> {
+        var cNode = node
+
+        if (isRed(cNode.right?.left)) {
+            cNode.right = cNode.right?.let { rotateRight(it) }
+            cNode = rotateLeft(cNode)
+            flipColor(cNode)
+        }
+
+        return cNode
+    }
+
+    private fun moveRedRight(node: Node<K, V>): Node<K, V> {
+        var cNode = node
+
+        flipColor(cNode)
+        if (isRed(cNode.left?.left)) {
+            cNode = rotateRight(cNode)
+            flipColor(cNode)
+        }
+
+        return cNode
+    }
+
+    private fun balance(node: Node<K, V>): Node<K, V> {
+        var cNode = node
+
+        if (isRed(cNode.right) && !isRed(cNode.left)) {
+            cNode = rotateLeft(cNode)
+        }
+        if (isRed(cNode.left) && isRed(cNode.left?.left)) {
+            cNode = rotateRight(cNode)
+        }
+        if (isRed(cNode.left) && isRed(cNode.right)) {
+            cNode = flipColor(cNode)
+        }
+
+        cNode.size = size(cNode.left) + size(cNode.right) + 1
+
+        return cNode
+    }
+
+    private fun min(node: Node<K, V>): Node<K, V> =
+        if (node.left == null) {
+            node
+        } else {
+            min(node.left!!)
+        }
+
+    private fun deleteMin(node: Node<K, V>): Node<K, V>? {
+        var cNode = node
+
+        if (cNode.left == null) {
+            return null
+        }
+
+        if (!isRed(cNode.left) && !isRed(cNode.left?.left)) {
+            cNode = moveRedLeft(cNode)
+        }
+
+        cNode.left = deleteMin(cNode.left!!)
+
+        return balance(cNode)
+    }
+
+    // Search
+    private fun get(key: K, node: Node<K, V>?): V? {
+        var cNode = node
+
+        while (cNode != null) {
+            if (key < cNode.key) {
+                cNode = cNode.left
+            } else if (key > cNode.key) {
+                cNode = cNode.right
+            } else {
+                return cNode.value
+            }
+        }
+
+        return null
+    }
+
+    fun get(key: K): V? {
+        return get(key, root)
+    }
+    fun contains(key: K): Boolean {
+        return get(key) != null
+    }
+
     // Insertion
     private fun put(key: K, value: V, node: Node<K, V>?): Node<K, V>? {
         if (node == null) {
             return Node(key, value, Color.RED, 1)
         }
 
-        var currentNode = node
-
-        val gap: Int = key.compareTo(currentNode.key)
+        val gap: Int = key.compareTo(node.key)
         if (gap > 0) {
-            currentNode.right = put(key, value, currentNode.right)
+            node.right = put(key, value, node.right)
         } else if (gap < 0) {
-            currentNode.left = put(key, value, currentNode.left)
+            node.left = put(key, value, node.left)
         } else {
-            currentNode.value = value
+            node.value = value
         }
 
-        if (isRed(currentNode.right) && !isRed(currentNode.left)) {
-            currentNode = rotateLeft(currentNode)
-        }
-        if (isRed(currentNode?.left) && isRed(currentNode?.left?.left)) {
-            currentNode = currentNode?.let { rotateRight(it) }
-        }
-        if (isRed(currentNode?.left) && isRed(currentNode?.right)) {
-            currentNode = currentNode?.let { flipColor(it) }
+        return balance(node)
+    }
+    fun put(key: K, value: V) {
+        root = put(key, value, root)
+        root?.color = Color.BLACK
+    }
+
+    // Deletion
+    private fun delete(key: K, node: Node<K, V>): Node<K, V>? {
+        var cNode = node
+
+        if (key < node.key) {
+            if (!isRed(cNode.left) && !isRed(cNode.left?.left)) {
+                cNode = moveRedLeft(cNode)
+            }
+            cNode.left = cNode.left?.left?.let { delete(key, it) }
+
+        } else {
+            if (isRed(cNode.left)) {
+                cNode = rotateRight(cNode)
+            }
+            if (key == cNode.key && !isRed(cNode.right?.left)) {
+                return null
+            }
+            if (!isRed(cNode.right) && !isRed(cNode.right?.left)) {
+                cNode = moveRedRight(cNode)
+            }
+
+            if (key == node.key) {
+                val temp = min(cNode.right!!)
+                cNode.key = temp.key
+                cNode.value = temp.value
+                cNode.right = deleteMin(cNode.right!!)
+            } else {
+                cNode.right = delete(key, cNode.right!!)
+            }
         }
 
-        currentNode?.size = size(currentNode?.left) + size(currentNode?.right) + 1
-
-        return currentNode
+        return balance(cNode)
     }
 }
