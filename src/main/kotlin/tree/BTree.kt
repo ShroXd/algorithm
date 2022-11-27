ef494b5842d32f5b6104706168f59dfb6abcb054
ShroXd <ShroXdF@gmail.com>
2022-11-27 16:17:53 +0800

    note: add some comments for the algorithm

--- src/main/kotlin/tree/BTree.kt
+++ src/main/kotlin/tree/BTree.kt
@@ -4,11 +4,8 @@ class BTree<K : Comparable<K>>(private val t: Int) {
     data class Node<K>(var isLeaf: Boolean) {
         var keys: MutableList<K> = mutableListOf()
         var children: MutableList<Node<K>> = mutableListOf()
-
-        var numOfKeys: Int = 0
     }
 
-    private val capacityOfKeys: Int = 2 * t - 1
     private var root: Node<K>? = null
 
     private fun search(key: K, node: Node<K>?): Node<K>? {
@@ -16,22 +13,27 @@ class BTree<K : Comparable<K>>(private val t: Int) {
             return null
         } else {
             var idx = 0
+            // When the while loop end, the index will be at right of the last element which is smaller than the key
+            // 1 5 10 14 20 key=18 22
             while (idx < node.keys.size && key > node.keys[idx]) {
                 idx += 1
             }
 
+            // Check if the key is the largest key in the array
+            // If so, the index will be equals to node.keys.size
             return if (idx == node.keys.size) {
                 search(key, node.children[idx - 1])
             } else if (key == node.keys[idx]) {
+                // Check if we find the key
                 node
             } else if (node.isLeaf) {
                 null
             } else {
+                // Jump into the children
                 search(key, node.children[idx])
             }
         }
     }
-
     fun search(key: K): Node<K>? {
         return search(key, root)
     }
@@ -42,11 +44,16 @@ class BTree<K : Comparable<K>>(private val t: Int) {
         val currentChild = node.children[idx]
         val newChild = Node<K>(currentChild.isLeaf)
 
+        // We need to push the middle key into the parent node
+        // Divide all child nodes equally
         newChild.keys = currentChild.keys.slice(0 until t).toMutableList()
         if (currentChild.isLeaf) {
             newChild.children = currentChild.children.slice(0 until t).toMutableList()
         }
 
+        // The structure of the b tree is
+        // node
+        // currentChild / newChild
         node.children.add(newChild)
         node.keys[t] = currentChild.keys[t]
 
@@ -77,6 +84,8 @@ class BTree<K : Comparable<K>>(private val t: Int) {
             // If the found child is full
             if (node.children[idx + 1].keys.size == 2 * t - 1) {
                 splitChild(idx + 1, node)
+                // After splitting, the middle key of child node will be put at the position with t index
+                // node.keys[t] = currentChild.keys[t]
                 if (node.keys[idx + 1] < key) {
                     idx += 1
                 }
@@ -92,12 +101,14 @@ class BTree<K : Comparable<K>>(private val t: Int) {
             temp.keys.add(key)
         } else {
             val currentRoot = root!!
+            // If the root node is full
             if (currentRoot.keys.size == 2 * t - 1) {
                 val newRoot = Node<K>(false)
                 newRoot.children.add(currentRoot)
                 splitChild(0, newRoot)
 
                 var idx = 0
+                // Determine if we need to put the key at the left of the current key or right
                 if (newRoot.keys[0] < key) {
                     idx += 1
                 }
